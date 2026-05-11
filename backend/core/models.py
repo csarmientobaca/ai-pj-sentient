8cf07302d58532aad1b453f5fb383b06d038693b
Carlos Sarmiento <c.sarmientobaca@gmail.com>
2026-05-11 18:20:44 +0200

    adde 5 more tries to new users

--- backend/core/models.py
+++ backend/core/models.py
@@ -4,7 +4,7 @@ from django.db.models.signals import post_save
 from django.dispatch import receiver
 
 
-TRIAL_INTERACTION_LIMIT = 5
+TRIAL_INTERACTION_LIMIT = 10
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="profile")
