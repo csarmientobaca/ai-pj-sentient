a9133757e881b0a3d49404f9b0662ca0e677309c
Carlos Sarmiento <c.sarmientobaca@gmail.com>
2026-05-15 01:25:29 +0200

    added profile in django admin

--- backend/core/admin.py
+++ backend/core/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Character, Memory, Interaction
+from .models import Character, Memory, Interaction, Profile
 
 admin.site.register(Character)
 admin.site.register(Memory)
 admin.site.register(Interaction)
+admin.site.register(Profile)
